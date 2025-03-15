ba95b99d6036a23477cf93d90d460482d0eeb3e0
Jesus Marenco <jesusmarencodev@gmail.com>
2025-03-15 15:57:50 -0500

    Config Nats

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,17 +4,23 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+
+  NATS_SERVERS: string[];
 }
 
 const envSchema = joi
   .object({
     PORT: joi.number().required(),
     DATABASE_URL: joi.string().required(),
+    NATS_SERVERS: joi.array().items(joi.string().required()),
   })
   .unknown(true);
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const { error, value } = envSchema.validate(process.env);
+const { error, value } = envSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
   throw new Error(`Config validation error : ${error.message}`);
@@ -26,4 +32,5 @@ const envVars: EnvVars = value;
 export const envs = {
   PORT: envVars.PORT,
   DATABASE_URL: envVars.DATABASE_URL,
+  NATS_SERVERS: envVars.NATS_SERVERS,
 };
